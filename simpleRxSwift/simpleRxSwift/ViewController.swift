15e02b33c33d9c68e4f7644d3ba68745ebde02a6
yamakentoc <yamakentoc@gmail.com>
2018-05-03 19:51:15 +0900

    first commit

---
+++ simpleRxSwift/simpleRxSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleRxSwift
+//
+//  Created by 山口賢登 on 2018/05/03.
+//  Copyright © 2018年 山口賢登. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
