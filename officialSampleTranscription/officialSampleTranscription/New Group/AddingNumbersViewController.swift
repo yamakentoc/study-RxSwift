d98c6aeaaa013db3efcbb03d062453fd2264930f
yamakentoc <yamakentoc@gmail.com>
2018-05-04 16:05:45 +0900

    add RxSwift RxCocoa

--- officialSampleTranscription/officialSampleTranscription/New Group/AddingNumbersViewController.swift
+++ officialSampleTranscription/officialSampleTranscription/New Group/AddingNumbersViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import RxCocoa
+import RxSwift
 
 class AddingNumbersViewController: UIViewController {
 
@@ -20,16 +22,5 @@ class AddingNumbersViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
