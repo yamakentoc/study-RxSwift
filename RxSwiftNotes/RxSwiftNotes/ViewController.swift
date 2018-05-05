8e5279d2723d29d42e3151e1c8127bbb985e4940
yamakentoc <yamakentoc@gmail.com>
2018-05-06 02:40:17 +0900

    add Variable Note

--- RxSwiftNotes/RxSwiftNotes/ViewController.swift
+++ RxSwiftNotes/RxSwiftNotes/ViewController.swift
@@ -54,3 +54,57 @@ let disposable = hoge.event?.subscribe(//購読開始
     .disposed(by: disposeBag)//購読完了
 
 
+
+///BehaviorSubject  現在地を取得できる。 ただしVariableの方が有能。
+///今の状態を反映した上で変化があったらそれを反映するという動作が実現できる
+class Presenter {
+    private let buttonHiddenSubject = BehaviorSubject(value: false)
+    var buttonHidden: Observable<Bool> { return buttonHiddenSubject }
+ 
+    func start() {
+        buttonHiddenSubject.onNext(true)
+    }
+    
+    func stop() {
+        buttonHiddenSubject.onNext(false)
+    }
+}
+
+let button = UIButton()
+let presenter = Presenter()
+let disposable2 = presenter.buttonHidden.subscribe(onNext: {[button] in
+    button.isHidden = $0
+})
+.disposed(by: disposeBag)
+
+
+/// Variable  BehaviorSubjectからObservableを取り除いたもの
+///BehaviorSubject と違ってonError / onCompleted を明示的に発生させることはできないため、現在値取得で例外が発生することはない。
+///bindTo メソッドの引数に指定できるというメリットもある
+class Presenter2 {
+    private let buttonHiddenVar = Variable(false)
+    var buttonHidden: Observable<Bool> { return buttonHiddenVar.asObservable() }
+    
+    func start() {
+        buttonHiddenVar.value = true
+    }
+    
+    func stop() {
+        buttonHiddenVar.value = false
+    }
+}
+
+class Config {
+    private let disposable: Disposable
+    let optionButtonEnabled = Variable(false)
+
+    init() {
+        disposable = optionButtonEnabled.asObservable()
+            .subscribe(onNext: { newValue in
+                // 値が設定されたときの処理
+        })
+    }
+    deinit {
+        disposable.dispose()
+    }
+}
