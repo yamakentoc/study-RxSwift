31fc80eb6b461193b8f1459abe3366d9875635bc
yamakentoc <yamakentoc@gmail.com>
2018-08-06 00:16:18 +0900

    各オブジェクトをコードに接続。 名前入力と自由入力のObservableを追加

--- VariousInput/VariousInput/ViewController.swift
+++ VariousInput/VariousInput/ViewController.swift
@@ -6,13 +6,34 @@
 //  Copyright © 2018 山口賢登. All rights reserved.
 //
 
+//このコードは、こちらのコードを写経しています。
+//https://qiita.com/fumiyasac@github/items/90d1ebaa0cd8c4558d96
+
 import UIKit
+import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var stateSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var freeTextField: UITextField!
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet var greetingButtons: [UIButton]!
+    
+    var lastSelectedGreeting: Variable<String> = Variable("こんにちは")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //nameTextFieldを観測対象に
+        let nameObservable: Observable<String?> = nameTextField.rx.text.asObservable()
+        //freeTextFieldを観測対象に
+        let freeObservable: Observable<String?> = freeTextField.rx.text.asObservable()
+        //combineLatest: nameTextFieldとfreeTextFieldの直近の最新値同士を結合する
+        let freewordWidtNameObservable: Observable<String?> = Observable.combineLatest(nameObservable, freeObservable) { (string1: String?, string2: String?) in
+            return string1! + string2!
+        }
+        
     }
 
 
