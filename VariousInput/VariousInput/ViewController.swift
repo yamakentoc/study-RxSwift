f4235f681615f35dd3894a9a8301441388f1d324
yamakentoc <yamakentoc@gmail.com>
2018-08-05 23:25:50 +0900

    create VariousInput

---
+++ VariousInput/VariousInput/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VariousInput
+//
+//  Created by 山口賢登 on 2018/08/05.
+//  Copyright © 2018 山口賢登. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
