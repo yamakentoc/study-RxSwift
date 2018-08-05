fdeb4f0d3f833e9ac936c5656353ed7b08295d3a
yamakentoc <yamakentoc@gmail.com>
2018-08-06 00:46:58 +0900

    SegmentedControlの状態の変化を検知して自由入力の場合はtextfieldを使えるようにした

--- VariousInput/VariousInput/ViewController.swift
+++ VariousInput/VariousInput/ViewController.swift
@@ -21,10 +21,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet var greetingButtons: [UIButton]!
     
+    //segmentedControlに対応する値の定義
+    enum State: Int {
+        case useButtons   //あいさつの始まり選択
+        case useTextField //自由入力にする
+    }
+    
+    let disposeBag = DisposeBag()
+    //初期値の設定
     var lastSelectedGreeting: Variable<String> = Variable("こんにちは")
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        addObservable()
+    }
+    
+    func addObservable() {
         //nameTextFieldを観測対象に
         let nameObservable: Observable<String?> = nameTextField.rx.text.asObservable()
         //freeTextFieldを観測対象に
@@ -34,8 +46,34 @@ class ViewController: UIViewController {
             return string1! + string2!
         }
         
+        //イベントのプロパティ接続をする
+        freeObservable
+            .bind(to: greetingLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        //SegmentedControlに値変化の値変化を観測対象に
+        let segmentedControlObservable: Observable<Int> = stateSegmentedControl.rx.value.asObservable()
+        
+        //SegmentedControlの値の変化を検知したらその状態に対応するenumの値を返す
+        //map 別の要素に変換する *IntからStateへ変換
+        let stateObservable: Observable<State> = segmentedControlObservable.map {
+            (selectedIndex: Int) -> State in
+            return State(rawValue: selectedIndex)!
+        }
+        
+        //enumの値の変化を検知して、テキストフィールドが編集を受け付けるかを返す
+        //mapはStateからBoolへ変換している
+        let greetingTextFieldEnabledObservable: Observable<Bool> = stateObservable.map {
+            (state: State) -> Bool in
+            return state == .useTextField
+        }
+        
+        //イベントのプロパティを接続
+        greetingTextFieldEnabledObservable
+            .bind(to: freeTextField.rx.isEnabled)
+            .disposed(by: disposeBag)
+                
+        
     }
-
-
 }
 
